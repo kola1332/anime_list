699d007588f5a40417c6417def18da1bd28ab786
Nikolai <kolmakovn111@gmail.com>
2023-04-09 19:15:04 +0300

    add images add 2 column

--- lib/main.dart
+++ lib/main.dart
@@ -8,19 +8,20 @@ void main() async {
   Dio dio = Dio();
   ApiService apiService = ApiService(dio);
   Anime anime = await apiService.get();
-  runApp( MainApp(anime: anime,));
+  runApp(MainApp(
+    anime: anime,
+  ));
 }
 
 class MainApp extends StatefulWidget {
-    Anime anime;
-  MainApp({super.key, required this.anime});
+  final Anime anime;
+  const MainApp({super.key, required this.anime});
 
   @override
   State<MainApp> createState() => _MainAppState();
 }
 
 class _MainAppState extends State<MainApp> {
-
   @override
   void initState() {
     loadAnime();
@@ -30,15 +31,71 @@ class _MainAppState extends State<MainApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner:  false,
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: ListView.builder(
-          itemCount: widget.anime.data!.length,
-          itemBuilder: (context, index) {
-            return ListTile(
-              title: Text(widget.anime.data![index].title),
-            );
-          },
+        body: SafeArea(
+          child: GridView.builder(
+            padding: const EdgeInsets.all(5),
+            itemCount: widget.anime.data!.length,
+            gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+              maxCrossAxisExtent: 200,
+              crossAxisSpacing: 3,
+              mainAxisSpacing: 10,
+              mainAxisExtent: 270,
+            ),
+            itemBuilder: ((context, index) {
+              return Container(
+                padding: const EdgeInsets.all(3),
+                height: 200,
+                width: 150,
+                child: Card(
+                    // semanticContainer: false,
+                    // clipBehavior: Clip.hardEdge,
+                    elevation: 5,
+                    // margin: const EdgeInsets.all(4),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      // mainAxisSize: MainAxisSize.min,
+                      children: [
+                        const SizedBox(height: 8),
+                        Container(
+                          padding: EdgeInsets.only(left: 8),
+                          child: Text(
+                            widget.anime.data![index].title,
+                            style: const TextStyle(
+                              fontSize: 13.5,
+                              fontWeight: FontWeight.w600,
+                            ),
+                            maxLines: 2,
+                          ),
+                        ),
+                        const SizedBox(height: 10),
+                        Container(
+                          height: 200,
+                          width: 140,
+                          padding: const EdgeInsets.all(7),
+                          decoration: BoxDecoration(
+                            shape: BoxShape.rectangle,
+                            boxShadow: <BoxShadow>[
+                              BoxShadow(
+                                color: Colors.grey.shade300,
+                                blurRadius: 1,
+                              ),
+                            ],
+                          ),
+                          child: Image.network(
+                            widget.anime.data![index].images!.jpg!.imageUrl,
+                            fit: BoxFit.fill,
+                            // scale: 0.5,
+                            // height: 150,
+                            // width: 150,
+                          ),
+                        ),
+                      ],
+                    )),
+              );
+            }),
+          ),
         ),
       ),
     );
@@ -51,3 +108,22 @@ class _MainAppState extends State<MainApp> {
     return anime;
   }
 }
+
+
+// ListView.builder(
+        //   itemCount: widget.anime.data!.length,
+        //   itemBuilder: (context, index) {
+        //     return GridView.count(
+        //       crossAxisCount: 2,
+        //       children: [
+        //         Text(widget.anime.data![index].title),
+        //         // ListTile(
+        //         //   title: Text(widget.anime.data![index].title),
+        //         // )
+        //       ],
+        //     );
+        //     // return ListTile(
+        //     //   title: Text(widget.anime.data![index].title),
+        //     // );
+        //   },
+        // ),
--- lib/models/anime.dart
+++ lib/models/anime.dart
@@ -5,20 +5,39 @@ class Anime {
     if (json['data'] != null) {
       data = <Data>[];
       json['data'].forEach((v) {
-        data!.add(new Data.fromJson(v));
+        data!.add(Data.fromJson(v));
       });
     }
   }
 }
 
 class Data {
+  Images? images;
   late int malId;
   late String title;
 
   Data(this.malId, this.title);
 
   Data.fromJson(Map<String, dynamic> json) {
+    images =
+        json['images'] != null ? Images.fromJson(json['images']) : null;
     malId = json['mal_id'];
     title = json['title'];
+  }
+}
+
+class Images {
+  Jpg? jpg;
+
+  Images.fromJson(Map<String, dynamic> json) {
+    jpg = json['jpg'] != null ? Jpg.fromJson(json['jpg']) : null;
+  }
+}
+
+class Jpg {
+  late String imageUrl;
+
+  Jpg.fromJson(Map<String, dynamic> json) {
+    imageUrl = json['image_url'];
   }
 }
